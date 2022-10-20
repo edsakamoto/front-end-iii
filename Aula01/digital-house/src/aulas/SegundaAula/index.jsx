9aa7aa1c44238c5c0f89ecd1796012c3a614ef13
edsakamoto <42415029+edsakamoto@users.noreply.github.com>
2022-10-19 22:53:26 -0300

    segundaAula

---
+++ Aula01/digital-house/src/aulas/SegundaAula/index.jsx
@@ -0,0 +1,17 @@
+import './style.scss'
+export function SegundaAula(){
+
+    return(
+        <main className="SegundaAula">
+
+            <h1>Segunda Aula</h1>
+            <h2>Projeto escolhido: Youtube</h2>
+            <ul>
+                <li>O header seria um componente: id="masthead-container"</li>
+                <li>Barra no lado esquerdo: id="items" class="style-scope ytd-app"</li>
+            </ul>
+
+        </main>
+    )
+
+}
---
+++ Aula01/digital-house/src/pages/Login/index.jsx
@@ -0,0 +1,6 @@
+export function Login(){
+
+    return( <h1 className="testeH1">Eu sou um componente de Login</h1> )
+
+}
+
